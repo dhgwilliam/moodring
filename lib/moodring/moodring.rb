4dbe6609057a42627874ec75a7c1f9c6e3d11f73
David Gwilliam <david@puppetlabs.com>
2013-09-21 01:03:31 -0400

    CLI client: add MoodringAPI#post and test command

--- lib/moodring/moodring.rb
+++ lib/moodring/moodring.rb
@@ -6,10 +6,15 @@ require 'httparty'
 
 
 MOODRING_URL = ENV['MOODRING_URL'] || 'localhost:4567'
+MOODRING_API = ENV['MOODRING_API'] || 'v1'
 
 class MoodringAPI
-  def self.get(endpoint, v = 'v1')
-    HTTParty.get('http://' + MOODRING_URL + "/api/#{v}/" + endpoint)
+  def self.get(endpoint)
+    HTTParty.get('http://' + MOODRING_URL + "/api/#{MOODRING_API}/" + endpoint)
+  end
+
+  def self.post(endpoint, options = {})
+    HTTParty.post('http://' + MOODRING_URL + "/api/#{MOODRING_API}/" + endpoint, options)
   end
 end
 
@@ -22,7 +27,18 @@ opts = Slop.parse({:help => true}) do
 
   command 'list' do
     run do
-      puts MoodringAPI.get('list')
+      puts MoodringAPI.get('list').body
+    end
+  end
+
+  command 'test' do
+    on :new, 'Initiate test'
+    on :user=, 'Supply user id'
+
+    run do |test_opts|
+      response = MoodringAPI.post("test/#{test_opts[:user]}/new")
+      puts response.code
+      puts response.body.to_s
     end
   end
 end
