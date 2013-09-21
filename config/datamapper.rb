3d6e85b59d0bb7117004065bf89015d11012621d
David Gwilliam <david@puppetlabs.com>
2013-09-21 16:30:36 -0400

    require dm-validations and dm-migrations

--- config/datamapper.rb
+++ config/datamapper.rb
@@ -1,5 +1,8 @@
 require 'dm-core'
+require 'dm-validations'
 require 'dm-redis-adapter'
+require 'dm-migrations'
 
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, {:adapter  => "redis"})
+DataMapper.auto_upgrade!
